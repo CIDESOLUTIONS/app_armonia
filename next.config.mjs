630534cacd3a9178fa063d0058c62cf1dd08afaa
Vicky Delgado <meciza@gmail.com>
2025-07-19 17:25:38 -0500

    fallas compilación

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   serverExternalPackages: ["@prisma/client"],
-  webpack: (config, { isServer }) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": `${__dirname}/src`,
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
